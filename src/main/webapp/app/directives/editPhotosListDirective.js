10f4ee188022c2693eb2f0989f3e1793958e276d
Alexander <dudko.av.89@gmail.com>
2015-12-29 14:45:10 +0200

    fixed carousel view

--- src/main/webapp/app/directives/editPhotosListDirective.js
+++ src/main/webapp/app/directives/editPhotosListDirective.js
@@ -5,16 +5,16 @@
         .module('friends_photos')
         .directive('editPhotosList', editPhotosListDirective);
 
-    editPhotosListDirective.$inject = ['$parse'];
+    editPhotosListDirective.$inject = ['$parse', '$mdDialog'];
 
-    function editPhotosListDirective($parse) {
+    function editPhotosListDirective($parse, $mdDialog) {
         var directive = {
             restrict: 'AE',
             template:
                 '<div flex ng-cloak>' +
-                    '<md-grid-list md-cols="6" md-row-height="130px"> ' +
+                    '<md-grid-list md-cols="6" md-row-height="130px" md-gutter="10px"> ' +
                         '<md-grid-tile ng-repeat="photo in photosList"> ' +
-                            '<img photo="photo" style="width: 100%; height: 100%;" />' +
+                            '<img photo="photo" style="width: 100%; height: 100%;" ng-click="showCarousel($event, null, photosList, $index)" />' +
                             '<md-grid-tile-footer><h3>Test</h3></md-grid-tile-footer>' +
                         '</md-grid-tile>' +
                     '</md-grid-list>' +
@@ -24,42 +24,14 @@
         return directive;
 
         function link(scope, elem, attrs) {
-            // var model = $parse(attrs.ngModel);
             angular.extend(scope, {
                 photosList: [],
-                getPhotoSrc: getPhotoSrc
+                showCarousel: $mdDialog.showCarousel
             });
-
-            function getPhotoSrc(photo) {
-
-            }
 
             scope.$watchCollection(attrs.photosList, function (list) {
                 scope.photosList = list;
             });
-
-/*            scope.$watchCollection(attrs.photosList, function (list) {
-                elem.empty();
-                angular.forEach(list, function (file) {
-                    var photoElem = angular
-                        .element('<img>')
-                        .addClass('img-thumbnail')
-                        .css({width: '100%', height: '130px', margin: '7px 0'});
-
-
-                    elem.append(photoElem.wrap('<div class="col-lg-2">').parent());
-
-                    if (file instanceof File) {
-                        var reader = new FileReader();
-                        reader.onload = function (e) {
-                            photoElem.attr('src', e.target.result);
-                        };
-                        reader.readAsDataURL(file);
-                    } else {
-                        photoElem.attr('src', 'photos/' + file + '/file');
-                    }
-                });
-            });*/
         }
     }
 
--- src/main/webapp/app/directives/eventPreviewDirective.js
+++ src/main/webapp/app/directives/eventPreviewDirective.js
@@ -5,9 +5,9 @@
         .module('friends_photos')
         .directive('eventPreview', eventPreviewDirective);
 
-    eventPreviewDirective.$inject = ['$mdDialog', '$mdMedia'];
+    eventPreviewDirective.$inject = ['$mdDialog'];
 
-    function eventPreviewDirective($mdDialog, $mdMedia) {
+    function eventPreviewDirective($mdDialog) {
         var directive = {
             restrict: 'AE',
             templateUrl: 'app/directives/templates/event-preview-directive.html',
@@ -20,41 +20,9 @@
 
         function link(scope, elem, attrs) {
             angular.extend(scope, {
-                showCarousel: showCarousel
+                showCarousel: $mdDialog.showCarousel
             });
-
-            function showCarousel(e) {
-                var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && scope.customFullscreen;
-                $mdDialog.show({
-                    //controller: DialogController,
-                    template:
-                        '<md-dialog class="carousel-modal" aria-label="List Photos">' +
-                            //'<md-dialog-content>' +
-                                '<carousel photos="event.photos" layout="row">' +
-                            //'</md-dialog-content>' +
-                        '</md-dialog>',
-                    //autoWrap: true,
-                    parent: angular.element(document.body),
-                    targetEvent: e,
-                    clickOutsideToClose: true,
-                    fullscreen: useFullScreen,
-                    scope: scope.$new()
-                }).then(function () {
-
-                }, function () {
-
-                });
-
-                // FIXME
-                scope.$watch(function () {
-                    return $mdMedia('xs') || $mdMedia('sm');
-                }, function(wantsFullScreen) {
-                    scope.customFullscreen = (wantsFullScreen === true);
-                });
-            }
         }
-
-
     }
 
 })();
